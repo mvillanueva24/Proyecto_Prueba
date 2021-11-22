58effe4fa97fb8235251189fb67cbb6dd9e73ba9
mvillanueva24 <mariorvillanueva24@gmail.com>
2021-11-22 04:16:57 -0500

    modelos

--- ProjectDjango/API/admin.py
+++ ProjectDjango/API/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categorias, Hoteles, TipoHabitacion, Habitaciones, Arduinos, Reserva, Cliente
+
+admin.site.register(Categorias)
+admin.site.register(Hoteles)
+admin.site.register(TipoHabitacion)
+admin.site.register(Habitaciones)
+admin.site.register(Arduinos)
+admin.site.register(Reserva)
+admin.site.register(Cliente)
